2c871aea3c055f4a2a0b7b130053382e702bdbc3
Krzysztof Czuryło <krzysztof.czurylo@gmail.com>
2017-11-15 19:38:46 +0100

    Improved the cleaning method to also check if obj has a body

--- feature_hashing.py
+++ feature_hashing.py
@@ -9,13 +9,13 @@ def removeArticlesWithoutTopic():
     global initialData, cleanData
     i = 0
     for obj in initialData:
-        if "topics" in obj:
+        if "topics" in obj and "body" in obj:
             cleanData.append(obj)
 
 
 if __name__ == '__main__':
     start_time = time.time()
     removeArticlesWithoutTopic()
-    pprint(cleanData[1])
+    pprint(len(cleanData))
     print("Execution time: %s seconds." % (time.time() - start_time))
 
